c48a80026e9802093e51a7134faf1e1a4b2587ff
Bkroland19 <bkroland19@gmail.com>
2023-02-04 07:21:21 +0300

    initial commit

---
+++ api/models/admin.js
@@ -0,0 +1,26 @@
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const adminSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Admin', adminSchema);
+
+
+
+
+
+
+
+
+
+
---
+++ client/src/pages/LandingPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import Features from '../components/Features';
+import Timeline from '../components/TimeLine';
+import Stats from '../components/StatsComponent';
+
+
+const LandingPage = () => {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Timeline />
+      <Stats />
+    </>
+  );
+};
+
+export default LandingPage;
